2bc328e40586c2bf244b988efc5a239fcb4f9f80
Nightingale111 <99151249+Nightingale111@users.noreply.github.com>
2024-10-26 08:18:45 -0400

    Update Source.cpp
    Wrote main function, addItemToList function, and printInventory function. 
    Need to fix error on line 24, code will not compile b/c setDiscount() is a non-static function and the scope resolution operator is trying to call it as if it were a static function. Looked at instructions and discountMultiplier has to be a static private member, and the main function has to be written as it is now.

--- Da Funky Monkee/Source.cpp
+++ Da Funky Monkee/Source.cpp
@@ -1,18 +1,53 @@
+#include "item.cpp"
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
-
-class Item {
+/*class Item {
 public:
 private:
 	string itemName;
 	int originalPrice;
 	static int discountMultiplier;
 
-};
+};*/
+
+void addItemsToList(vector<Item>& inventory);
+void printInventory(vector<Item> inventory);
 
 int main(){
+	vector<Item> inventory;
+	addItemsToList(inventory);
 
+	Item::setDiscount(20);
 
+	printInventory(inventory);
+
+}
+
+void addItemsToList(vector<Item>& inventory) {
+	int itemNumber;
+	cout << "How many items would you like to add to the inventory? ";
+	cin >> itemNumber;
+	for (int item = 0; item < itemNumber; item++) {
+		string name;
+		double price;
+		cout << "\n\nItem #" << (item + 1) <<"\nWhat is the item name? ";
+		cin >> name;
+		cout << "\nWhat is the item price? ";
+		cin >> price;
+
+		Item newItem(name, price, 0);
+		inventory.push_back(newItem);
+	}
+}
+void printInventory(vector<Item> inventory) {
+	cout << "\nInventory:\n\n";
+	for (Item item : inventory) {
+		cout << "Name: " << item.getName();
+		cout << "\nOriginal Price: $" << item.getOPrice();
+		cout << "\nDiscounted Price: $" << item.getDiscPrice();
+	}
 }
